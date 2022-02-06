1e65b5d301d00320fb16e606f0f447c2fb1063d2
Lamek Marouf <lamekmaroufjob@hotmail.com>
2022-02-06 13:29:26 +1100

    added packages needed for this application

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,8 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 
 // TODO: Create an array of questions for user input
