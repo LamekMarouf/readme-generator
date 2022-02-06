362a4d0742acebdd24077e4a6885ea3a042ee7e4
Lamek Marouf <lamekmaroufjob@hotmail.com>
2022-02-06 13:29:49 +1100

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -7,7 +7,83 @@ const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 
-const questions = []
+const questions = [
+    {
+        type: "input",
+        message: "What's the project title",
+        name: "title",
+        validate: (value) =>{ if(value){return true} else {return "I need a title!"}},
+    },
+
+    {
+        type: "input",
+        message: "What is the description of your project?",
+        name: "description",
+        validate: (value) =>{ if(value){return true} else {return "I need a description!"}},
+    },
+
+    {
+        type: "input",
+        message: "Table of Contents",
+        name: "contents",
+    },
+
+    {
+        type: "input",
+        message: "What does the user need to install to use your app?",
+        name: "installation",
+        validate: (value) =>{ if(value){return true} else {return "I need a value!"}},
+    },
+
+    {
+        type: "input",
+        message: "How do you use your app?",
+        name: "usage",
+        validate: (value) =>{ if(value){return true} else {return "I need a value!"}},
+    },
+
+    {
+        type: "input",
+        message: "What license is being used?",
+        name: "license",
+        validate: (value) =>{ if(value){return true} else {return "I need a value!"}},
+    },
+
+    {
+        type: "input",
+        message: "Who contributed to this project?",
+        name: "contribution",
+    },
+
+    {
+        type: "input",
+        message: "What commands are needed to test this app?",
+        name: "tests",
+        validate: (value) =>{ if(value){return true} else {return "I need a value!"}},
+    },
+
+    {
+        type: "input",
+        message: "Contact info for inquiries",
+        name: "questions",
+        validate: (value) =>{ if(value){return true} else {return "I need a value!"}},
+    },
+
+    {
+        type: "input",
+        message: "GitHub username:",
+        name: "git",
+        validate: (value) =>{ if(value){return true} else {return "I need a value!"}},
+    },
+
+    {
+        type: "input",
+        message: "Email:",
+        name: "email",
+        validate: (value) =>{ if(value){return true} else {return "I need a value!"}},
+    }
+
+]
 
 
 // // TODO: Create a function to write README file
